0f878a7ee51ed9248101a47749a14d629f15cf9d
Tanapat Namsomboon <tanapat.nams@outlook.com>
2026-04-17 13:48:27 +0700

    feat: integrate GLM math library and implement orthographic camera

---
+++ engine/include/loom/renderer/orthographic_camera.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "loom/core/core.h"
+#include <glm/glm.hpp>
+
+namespace Loom {
+
+    class LOOM_API OrthographicCamera {
+    public:
+        OrthographicCamera(float left, float right, float bottom, float top);
+
+        void SetProjection(float left, float right, float bottom, float top);
+
+        const glm::vec3& GetPosition() const { return mPosition; }
+        void SetPosition(const glm::vec3& position) { mPosition = position; RecalculateViewMatrix(); }
+
+        float GetRotation() const { return mRotation; }
+        void SetRotation(float rotation) { mRotation = rotation; RecalculateViewMatrix(); }
+
+        const glm::mat4& GetProjectionMatrix() const { return mProjectionMatrix; }
+        const glm::mat4& GetViewMatrix() const { return mViewMatrix; }
+        const glm::mat4& GetViewProjectionMatrix() const { return mViewProjectionMatrix; }
+
+    private:
+        void RecalculateViewMatrix();
+
+    private:
+        glm::mat4 mProjectionMatrix;
+        glm::mat4 mViewMatrix;
+        glm::mat4 mViewProjectionMatrix;
+
+        glm::vec3 mPosition = { 0.0f, 0.0f, 0.0f };
+        float mRotation = 0.0f;
+    };
+
+} // namespace Loom
